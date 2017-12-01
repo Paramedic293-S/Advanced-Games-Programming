f3d307321f60abb0ce1d88eb3dd9012d64f66692
Paramedic293 <Paramedic292@hotmail.co.uk>
2017-12-01 02:38:59 +0000

    Tutorials 4 to 7
    Tutorials 4 to 7

---
+++ Mark Lizzimore CGP600 Tutorial 4 E2/shaders.hlsl
@@ -0,0 +1,31 @@
+cbuffer CBuffer0
+{
+	float red_fraction;
+	float Scale;
+	float2 packing;
+};
+
+struct VOut
+{
+	float4 position: SV_POSITION;
+	float4 color : COLOR;
+};
+
+VOut VShader(float4 position : POSITION, float4 color : COLOR)
+{
+	VOut output;
+
+	color.r *= red_fraction;
+	output.position = position;
+	/*output.position.x *= Scale;
+	output.position.y *= (1.0 - Scale);*/
+	output.position.xy *= Scale;
+	output.color = color;
+
+	return output;
+}
+
+float4 PShader (float4 position : SV_POSITION, float4 color : COLOR) : SV_TARGET
+{
+	return color;
+}
---
+++ Mark Lizzimore CGP600 Tutorial 7/shaders.hlsl
@@ -0,0 +1,35 @@
+cbuffer CBuffer0
+{
+	matrix WVPMatrix;
+	matrix WVPMatrix2;
+	float red_fraction;
+	float Scale;
+	float2 packing;
+};
+
+struct VOut
+{
+	float4 position: SV_POSITION;
+	float4 color : COLOR;
+};
+
+VOut VShader(float4 position : POSITION, float4 color : COLOR)
+{
+	VOut output;
+
+	color.r *= red_fraction;
+	/*output.position = position;
+	output.position.x *= Scale;
+	output.position.y *= (1.0 - Scale);
+	output.position.xy *= Scale;*/
+	output.position = mul(WVPMatrix, position);
+	/*output.position = mul(WVPMatrix2, position);*/
+	output.color = color;
+
+	return output;
+}
+
+float4 PShader (float4 position : SV_POSITION, float4 color : COLOR) : SV_TARGET
+{
+	return color;
+}
